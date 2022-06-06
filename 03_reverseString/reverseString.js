529f867566a372919c8dd1c836d4c9d92f6643be
Eren Gazioglu <pompyproduction@gmail.com>
2022-06-06 23:44:21 +0100

    Solve exercises 01 through 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,9 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let revStr = "";
+    for (let i = 1; i <= str.length; i++) {
+        revStr = revStr.concat(str.substr(-i).charAt(0));
+    }
+    return revStr
 
 };
 
