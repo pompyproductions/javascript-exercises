3b51f50f00d42c8dc25bb4f07c84269c082634ac
Eren Gazioglu <pompyproduction@gmail.com>
2022-06-07 12:04:30 +0100

    Solve exercises 07 through 09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let result = 0;
+  array.forEach((item) => {
+    result += item;
+  })
+	return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = array.pop();
+  array.forEach((item) => {
+    result *= item;
+  })
+	return result;
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+  let result = 1;
+  for (let i = 0; i < power; i++) {
+    result *= number;
+  }
+	return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let result = 1;
+  for (let i = 2; i <= number; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    str = str.replaceAll(/[\s!?':;,.]/g, "");
+    return str == reverseString(str);
 };
+
+const reverseString = function(str) {
+    let revStr = "";
+    for (let i = 1; i <= str.length; i++) {
+        revStr = revStr.concat(str.substr(-i).charAt(0));
+    }
+    return revStr;
+};
+
 
 // Do not edit below this line
 module.exports = palindromes;
+
+palindromes("Anana");
+palindromes("anastas mum satSaNa!!")
