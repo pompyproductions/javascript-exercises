01b7eb7399336c051e4a6e3753a0c0d324bbf31a
Eren Gazioglu <pompyproduction@gmail.com>
2022-06-07 14:16:22 +0100

    Solve exercises 10 through 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const fibonacci = function() {
-
+const fibonacci = function(pos) {
+    if (pos < 1) return "OOPS";
+    if (pos <= 2) return 1;
+    return fibonacci(pos-1) + fibonacci(pos-2);
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let oldest = array.pop();
+    array.forEach(element => {
+        if (getAge(element) > getAge(oldest)) oldest = element;
+    });
+    return oldest;    
+};
 
+// returns NEGATIVE BIRTH DATE
+const getAge = function(item) {
+    let today = new Date()
+    today = today.getFullYear();
+    return (item.hasOwnProperty("yearOfDeath")) 
+        ? item.yearOfDeath - item.yearOfBirth 
+        : today - item.yearOfBirth;
 };
 
 // Do not edit below this line
